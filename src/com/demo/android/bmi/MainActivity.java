af1907afaff707c9fc0fee4275d8f4896f56e1b6
gasolin <gasolin@gmail.com>
2013-03-09 23:13:25 +0800

    refactor by CH10

--- src/com/demo/android/bmi/MainActivity.java
+++ src/com/demo/android/bmi/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,31 +17,47 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//Listen for button clicks
-        Button button = (Button)findViewById(R.id.submit);
-        button.setOnClickListener(calcBMI);
+		initViews();
+	    setListensers();
 	}
 
-	private OnClickListener calcBMI = new OnClickListener() { 
+	private Button button_calc;
+	private EditText num_height;
+	private EditText num_weight;
+	private TextView show_result;
+	private TextView show_suggest;
+	
+	private void initViews() {
+	  button_calc = (Button) findViewById(R.id.submit);
+	  num_height = (EditText) findViewById(R.id.height);
+	  num_weight = (EditText) findViewById(R.id.weight);
+	  show_result = (TextView) findViewById(R.id.result);
+	  show_suggest = (TextView) findViewById(R.id.suggest);
+	}
+
+	// Listen for button clicks
+	private void setListensers() {
+	    button_calc.setOnClickListener(calcBMI);
+    }
+	
+	private Button.OnClickListener calcBMI = new Button.OnClickListener() { 
         public void onClick(View v) {
             DecimalFormat nf = new DecimalFormat("0.00");
-            EditText fieldheight = (EditText)findViewById(R.id.height);
-            EditText fieldweight = (EditText)findViewById(R.id.weight);
-            double height = Double.parseDouble(fieldheight.getText().toString())/100;
-            double weight = Double.parseDouble(fieldweight.getText().toString());
+            double height = Double.parseDouble(num_height.getText().toString()) / 100;
+            double weight = Double.parseDouble(num_weight.getText().toString());
             double BMI = weight / (height * height);
 
-            TextView result = (TextView)findViewById(R.id.result);
-            result.setText("Your BMI is "+nf.format(BMI));
+            // Present result
+            show_result.setText(getText(R.string.bmi_result) + nf.format(BMI));
 
-            //Give health advice
-            TextView fieldsuggest = (TextView)findViewById(R.id.suggest);
+            // Give health advice
+            TextView txt_suggest = (TextView)findViewById(R.id.suggest);
             if (BMI > 25) {
-             fieldsuggest.setText(R.string.advice_heavy);
+            	txt_suggest.setText(R.string.advice_heavy);
             } else if (BMI < 20) {
-             fieldsuggest.setText(R.string.advice_light);
+            	txt_suggest.setText(R.string.advice_light);
             } else {
-             fieldsuggest.setText(R.string.advice_average);
+            	txt_suggest.setText(R.string.advice_average);
             }
         }
     };
